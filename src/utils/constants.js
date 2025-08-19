c55583ca3d6e77b55444a0918398153d3a0b60ac
Mrunal <mrunalkumbhare007@gmail.com>
2025-08-19 23:51:30 +0530

    changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-// export const BASE_URL = "https://devtinder-backend-76ef6d93ec58.herokuapp.com"
+// export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const BASE_URL = "https://devtinder-backend-76ef6d93ec58.herokuapp.com"
 // export const BASE_URL = "http://localhost:7777"
